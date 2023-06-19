d9d1eb97967565a87a86bba8b0a67f7f0c2272f8
DiegoDeJotaWeb <diego.rodrigues.joaquim@gmail.com>
2023-06-19 17:23:36 -0300

    Página index (login usuario)

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<div class="container">
+    <form action="">
+        <label for="">Login</label>
+        <input type="text" name="nome">
+        <label for="">Senha</label>
+        <input type="password" name="senha">
+    </form>
+</div>
+    
+</body>
+</html>
